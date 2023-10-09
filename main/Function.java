29c86073f4211348dc2c437e27a999ad8f60d078
jmarualgit <jairelmarual@gmail.com>
2023-10-09 16:51:32 -0700

    Cleaned up Function.java
    Removed commented-out functions from previous versions

--- main/Function.java
+++ main/Function.java
@@ -90,63 +90,6 @@ public class Function {
         return answer;
     }
 
-    /*
-    static double add(double input1, double input2) {
-
-        return input1 + input2;
-    }
-
-    static double subtract(double input1, double input2) {
-        return input1 - input2;
-    }
-
-    static double multiply(double input1, double input2) {
-        return input1 * input2;
-    }
-
-    static double divide(double input1, double input2) {
-
-        // dividing by 0 is bad
-        if (input2 == 0) {
-            return 0.0;
-        } else {
-            return input1 / input2;
-        }
-    }
-
-    
-
-    String deleteLeadingZeros(String string) {
-
-        int position = 0;
-        int stringLength = string.length();
-
-        // while the position isn't at the last character and the character at the position is still 0
-        while ((position < (stringLength - 1)) && (string.charAt(position) == '0')) {
-            
-            // move up across the screen
-            position++;
-        }
-
-        string = string.substring(position);
-
-        return string;
-    }
-
-    public String convertFromPercent(String string) {
-
-        // converts to a double named number
-        double number = Double.parseDouble(string);
-
-        // multiplies number by 100 (which is = moving decimal to the right 2x)
-        // reversing the 'percentagification'
-        number *= 100;
-
-        return String.valueOf(number);
-    }
-
-    */
-
     public String percentConvert(String string, boolean isPercent) {
         // converts to double named number
         double number = Double.parseDouble(string);
